e88f8c67672672c419ac3ad91b9e07d2740bba32
KatsuoTomino <to.katufumi.629@gmail.com>
2025-05-08 22:09:50 +0900

    エラー修正6回目

--- src/app/api/checkout/success/route.ts
+++ src/app/api/checkout/success/route.ts
@@ -5,7 +5,7 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 //購入履歴の保存
-export async function POST(request: Request, response: Response) {
+export const POST = async (request: Request) => {
   const { sessionId } = await request.json();
 
   try {
@@ -31,4 +31,4 @@ export async function POST(request: Request, response: Response) {
   } catch (err: any) {
     return NextResponse.json(err);
   }
-}
+};
