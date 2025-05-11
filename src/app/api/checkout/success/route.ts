5f15ee44e28803caa525f899020a5c5d7630591f
KatsuoTomino <to.katufumi.629@gmail.com>
2025-05-11 21:22:17 +0900

    エラー修正

--- src/app/api/checkout/success/route.ts
+++ src/app/api/checkout/success/route.ts
@@ -4,24 +4,35 @@ import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
-//購入履歴の保存
+// 購入履歴の保存
 export const POST = async (request: Request) => {
   const { sessionId } = await request.json();
 
   try {
     const session = await stripe.checkout.sessions.retrieve(sessionId);
 
+    const userId = session.client_reference_id;
+    if (!userId) {
+      throw new Error("client_reference_id is undefined or null");
+    }
+
+    const bookId = session.metadata?.bookId ?? "defaultBookId";
+    if (!bookId) {
+      throw new Error("bookId is undefined or null");
+    }
+
     const existingPurchase = await prisma.purchase.findFirst({
       where: {
-        userId: session.client_reference_id!,
-        bookId: session.metadata?.bookId ?? "defaultBookId", // 安全なフォールバック
+        userId: userId,
+        bookId: bookId,
       },
     });
+
     if (!existingPurchase) {
       const purchase = await prisma.purchase.create({
         data: {
-          userId: session.client_reference_id!,
-          bookId: session.metadata?.bookId!,
+          userId: userId,
+          bookId: bookId,
         },
       });
       return NextResponse.json({ purchase });
@@ -29,6 +40,6 @@ export const POST = async (request: Request) => {
       return NextResponse.json({ message: "すでに購入済みです" });
     }
   } catch (err) {
-    return NextResponse.json(err);
+    return NextResponse.json({ error: "An unexpected error occurred." });
   }
 };
